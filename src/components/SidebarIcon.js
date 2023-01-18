8f68b2e164366378825bf9e0025d82b2c9902828
YutoMiyata <yuto.miya525ta@gmail.com>
2023-01-18 20:26:44 +0900

    reactでサイドバーを作成

---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import icon from "../images/S__4087816.jpg"
+
+function SidebarIcon() {
+  return (
+    <div className='SidbarIcon'>
+      <img src={icon}/>
+      <p>yuuuutoooon@gmail.com</p>
+    </div>
+  )
+}
+
+export default SidebarIcon
