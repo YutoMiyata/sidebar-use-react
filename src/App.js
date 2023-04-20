5885fe19f58c4d2214f1661312990e6071d8aa35
YutoMiyata <yuto.miya525ta@gmail.com>
2023-04-20 23:19:44 +0900

    Section.jsの追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Section from './components/ Section';
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <Section />
     </div>
   );
 }
